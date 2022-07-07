02753be28bfa6b6fb8541d984f6e34cd776ea209
zachariah-dawood <zachariah.dawood.ext@siemens.com>
2022-07-07 16:46:02 -0400

    added node context sensitivity

--- src/DDDNetwork.jsx
+++ src/DDDNetwork.jsx
@@ -2,14 +2,12 @@ import { Component, createElement } from "react";
 import "./ui/DDDNetwork.css";
 import { Graph } from "react-d3-graph";
 
-//the data and myConfig consts are hard-coded until I can access the data from the 
-//entities. When that's accessible, this page will probably convert 
-//the ListValue into the data format the graph class is expecting and then call it.  
+//the data below is an example of how inputs for the data argument of the graph function should be structured  
 const data = {
-    nodes: [{ id: "Harry" }, { id: "Sally" }, { id: "Alice" }, { id: "Bob" },],
+    nodes: [{ id: "Harr" }, { id: "Sally" }, { id: "Alice" }, { id: "Bob" },],
     links: [
-      { source: "Harry", target: "Sally" },
-      { source: "Harry", target: "Alice" },
+      { source: "Harr", target: "Sally" },
+      { source: "Harr", target: "Alice" },
       { source: "Alice", target: "Bob" }
     ],
   };
@@ -44,12 +42,36 @@ const data = {
 
 export class DDDNetwork extends Component {
     render() {
+      if(this.props.nodes.status === "available" &&
+      this.props.links.status === "available"){
+        var info = {};
+        var nodesinfo = new Array();
+        for(let i = 0; i < this.props.nodes.items.length; i++){
+          let newObj = {};
+          newObj.id = this.props.nodeName.get(this.props.nodes.items[i]).displayValue;
+          nodesinfo.push(newObj);
+        }
+        info.nodes = nodesinfo;
+        // the links get very upset if there's a typo. If you're getting a 
+        // "you provided a [sic] invalid data structure" error, make sure that 
+        // all your source and target ids match their node names 
+        var linksinfo = new Array();
+        for(let j = 0; j < this.props.links.items.length; j++){
+          let newerObj = {};
+          newerObj.source = this.props.linkSourceID.get(this.props.links.items[j]).displayValue;
+          newerObj.target = this.props.linkTargetID.get(this.props.links.items[j]).displayValue;
+          linksinfo.push(newerObj);
+        }
+        info.links = linksinfo;
         return <Graph
         id="graph-id"
-        data={data}
+        data={info}
         config={Config}
         onClickNode={onClickNode}
         onClickLink={onClickLink}
       />;
+      } else {
+        return null;
+      }
     }
 }
--- src/components/DDDNetworkJSON.js
+++ src/components/DDDNetworkJSON.js
@@ -1,3 +1,57 @@
-import React, {useRef, useEffect} from "react";
+import { Component, createElement } from "react";
+import "./ui/DDDNetwork.css";
 import { Graph } from "react-d3-graph";
 
+//the data and myConfig consts are hard-coded until I can access the data from the 
+//entities. When that's accessible, this page will probably convert 
+//the ListValue into the data format the graph class is expecting and then call it.  
+const data = {
+    nodes: [{ id: "George" }, { id: "Sally" }, { id: "Alice" }, { id: "Bob" },],
+    links: [
+      { source: "George", target: "Sally" },
+      { source: "George", target: "Alice" },
+      { source: "Alice", target: "Bob" }
+    ],
+  };
+
+  var w = innerWidth;
+  const Config = {
+    directed: true,
+    panAndZoom: true,
+    width: w,
+    collapsible: true,
+    nodeHighlightBehavior: true,
+    node: {
+      color: "lightgreen",
+      size: 120,
+      highlightStrokeColor: "blue",
+    },
+    link: {
+      highlightColor: "lightblue",
+    },
+    d3: {
+      gravity: -100,
+    }
+  };
+  
+  const onClickNode = function(nodeId) {
+    window.alert(`Clicked node ${nodeId}`);
+  };
+  
+  const onClickLink = function(source, target) {
+    window.alert(`Clicked link between ${source} and ${target}`);
+  };  
+
+export class DDDNetwork extends Component {
+    
+    
+    render() {
+        return <Graph
+        id="graph-id"
+        data={data}
+        config={Config}
+        onClickNode={onClickNode}
+        onClickLink={onClickLink}
+      />;
+    }
+}
